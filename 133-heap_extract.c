9febfeb2ca1a4037c802e0313956475174999a71
Isaac Phiri <isaacphiri315@gmail.com>
2022-11-20 10:33:21 +0000

    Advanced tasks

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+* heap_extract - pending.
+* @root: pending.
+* Return: 1 or 0
+*/
+int heap_extract(heap_t **root)
+{
+	if (!root || !*root)
+		return (0);
+	return (1);
+}
